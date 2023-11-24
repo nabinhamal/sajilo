26ea244f07402c1a9013bdebe445a8c02b2e6d2d
Nabin Hamal <nabinhamal2@gmail.com>
2023-11-24 10:17:28 +0545

    mongodb added

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config(); 
 
+mongoose.connect(process.env.MONGO)
+.then(() =>{
+    console.log('connected to MongoDB')
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
- const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running om port ${PORT}`)
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running om port ${process.env.PORT}`)
 })
