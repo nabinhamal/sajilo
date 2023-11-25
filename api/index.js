b816b51fd0ac55005cbf1a4cd745a699b1fa30e1
Nabin Hamal <nabinhamal2@gmail.com>
2023-11-25 09:39:46 +0545

    created signup api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv';
 dotenv.config(); 
 
@@ -12,8 +14,12 @@ mongoose.connect(process.env.MONGO)
 })
 
 const app = express();
-
+app.use(express.json())
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running om port ${process.env.PORT}`)
-})
+});
+
+
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
